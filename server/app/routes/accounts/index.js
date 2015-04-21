a11b0782ab2754f1f76b1810b657633b4904d3cd
Christian Cueto <ccueto90@gmail.com>
2015-04-21 18:16:08 -0400

    Accounts: found user and associated order

--- server/app/routes/accounts/index.js
+++ server/app/routes/accounts/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var router = require('express').Router();
 var User = require('mongoose').model('User');
+var Order = require('mongoose').model('Order');
 
 router.post('/create', function(req, res, next){
 	User.create(req.body).then(fulfilled, rejected);
@@ -18,16 +19,28 @@ router.get('/details', function(req,res,next){
 
 	var user_id = req.session.passport.user;
 	// console.log(user_id);
-
-	User.findOne({_id:user_id}).exec()
+	var foundUser;
+	var foundOrders;
+	User.findOne({_id:user_id}).exec().then(findOrder)
 		.then(fulfilled, rejected);
 		//TODO: need to look up order based on user_id
 
-	function fulfilled(user){
+	function findOrder(user){
+		//TODO: implement finding multiple orders
+		foundUser = user;
 		// console.log(user);
+		var orderId = user.orders[0];
+		return Order.findOne({_id: orderId}).exec().then(function(orders){
+			// console.log(orders);
+			foundOrders = orders;
+		});
+	}
+
+	function fulfilled(){
+		// console.log(orders);
 		//TODO: Only sending back email, do not send back the password salt
 		// console.log(user);
-		res.send({email:user.email});
+		console.log({user:foundUser.email, orders: foundOrders});
 		//TODO: Also grab orders based on user id and send this back
 	}
 	function rejected(err){
