542450f92d63a61c115d5ed5669dcc81b137d819
Ash Ryan <ash@ashryanbeats.com>
2015-04-22 16:31:34 -0400

    Testing: Passing all tests for validation errors, success, and user association.

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 
+
+function minLength(str) {
+	return str.length > 40;
+}
+var minLengthWithMessage = [minLength, "Input text is too short"];
+
 var schema = new mongoose.Schema({
-	stars: Number,
-	text: String,
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+	stars: 	{ type: Number, required: true, min: 1, max: 5},
+	text: 	{ type: String, required: false, validate: minLengthWithMessage },
+	user: 	{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }
 });
+
 
 mongoose.model('Review', schema);
